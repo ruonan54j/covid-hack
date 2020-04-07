37938eb2ffc217078ce86c6cabcea00beb475aff
tripoloski1337 <arsalan.dp@gmail.com>
2020-04-07 23:53:13 +0700

    adding firebase config  & connection

--- server/config/env.js
+++ server/config/env.js
@@ -3,7 +3,7 @@
 exports.Port      = 8000;
 exports.Origin    = "*";
 
-/* config for database */
+/* config for mongo */
 exports.DbHost      = "mongodb://localhost:27017/covid-hack";
 exports.DbName      = "covid-hack";
 exports.DbUser      = "root";
@@ -14,6 +14,15 @@ exports.DbOptions   = {
     useUnifiedTopology: true,
     useFindAndModify: false
 };
+
+/* config for firebase */
+exports.FIRE_API_KEY     = "";
+exports.FIRE_AUTH_DOMAIN = "";
+exports.FIRE_DB_URL      = "";
+exports.FIRE_PROJECT_ID  = "";
+exports.FIRE_STORAGE_BUCK= "";
+exports.FIRE_MSG_SENDERID= "";
+exports.FIRE_APP_ID      = "";
 
 /* config for security */
 exports.KEY       = "ahhthisismyverysecretcodeyeahhh!!!!!"
---
+++ server/database/db.firebase.js
@@ -0,0 +1,15 @@
+var config      = require("../config/env");
+var firebase    = require('firebase');
+
+var config = {
+    apiKey: config.FIRE_API_KEY,
+    authDomain: config.FIRE_AUTH_DOMAIN,
+    databaseURL: config.FIRE_DB_URL,
+    projectId: config.FIRE_PROJECT_ID,
+    storageBucket: config.FIRE_STORAGE_BUCK,
+    messagingSenderId: config.FIRE_MSG_SENDERID,
+    appId: config.FIRE_APP_ID
+}
+
+var fire = firebase.initializeApp(config);
+module.exports = fire;
