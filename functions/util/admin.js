f51f31f839d388edd870f27c67317fc0aab76d25
Sathyanarayanan Govindarajan <sathyagovindarajan28@gmail.com>
2020-04-15 11:12:09 -0500

    Set up Firebase Admin Service Account
    Added Admin Service Account in admin.js
    Key is private and marked in gitignore.

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,10 @@
-const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
+const admin = require("firebase-admin");
+const serviceAccount = require('./firebaseAdminKey.json');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://covid-hack-c6549.firebaseio.com"
+});
+const db = admin.firestore();
 module.exports = {admin, db};
 
